ea09a17a3e7a43bc3e54823d07e09acad064719e
Philipp Dikmann <mail@philippdikmann.de>
2017-09-01 21:14:08 +0200

    remove dead code, index elements

--- src/otto_cat2/core.cljs
+++ src/otto_cat2/core.cljs
@@ -8,10 +8,8 @@
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:text "Hello world!"
-                          ;;:imgs []
                           :objs []
                           :thing {:a 1 :b 2}}))
-
 
 (defn new-objs [n]
   (let [base-url "https://i.otto.de/i/otto/"
@@ -33,47 +31,20 @@
                   big-urls)]
     maps))
 
-#_(defn new-imgs [n]
-    (let [base-url "https://i.otto.de/i/otto/"
-          size-param "?w=200&h=200"
-          lower-limit 10000
-          upper-limit 20000000
-          random-ints (for [x (range n)]
-                        (+ (.floor js/Math
-                                   (* (.random js/Math)
-                                      (- upper-limit lower-limit)))
-                           lower-limit))
-          urls (map #(str base-url % size-param)
-                    random-ints)]
-      urls
-      ))
-
 (defn hello-world []
   [:div
-   ;;[:h1 (:text @app-state)]
-   ;;[:h3 "Edit this and watch it change!"]
-   [:p (:a (:thing @app-state)) ;;(get-in @app-state [:thing :a])
-    ]
-   [:p "fuck"]
    [:a {:href "#"
         :onClick (fn []
-                   ;;(swap! app-state assoc-in [:imgs] (new-imgs 3))
-                   (swap! app-state assoc-in [:objs] (new-objs 3))
+                   (swap! app-state assoc-in [:objs] (new-objs 50))
                    (println "shiiiiit")
                    (println "what" @app-state)
                    )}
     "click to shuffle"]
-   ;; [:hr]
-   ;; (for [url (:imgs @app-state)]
-   ;;   ^{:key url}
-   ;;   [:div {:class "inline"}
-   ;;    [:img {:src url}]
-   ;;    [:span {:class "tiny-font"} url]])
    [:hr]
    (for [img (:objs @app-state)]
-     ;;^{:key (:n img)}
+     ^{:key (:n img)}
      [:div {:class "inline"}
-      [:a {:href (:big img) :target "blank"}
+      [:a {:href (:big img) :target "_blank"}
        [:img {:src (:url img)}]]
       [:br]
       [:p {:class "tiny-font center"}
